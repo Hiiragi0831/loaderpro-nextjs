dbc7c82ed05a0ad8323a0bc3668eebc008164d26
Hiiragi0831 <glazunovden@yandex.ru>
2024-09-04 22:23:56 +0300

    fix

--- src/components/AuthModal.tsx
+++ src/components/AuthModal.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { FC } from "react";
 import { Modal } from "@/components/Modal";
 import { SignIn } from "@/components/forms/sign-in";
--- src/components/ProfileButton.tsx
+++ src/components/ProfileButton.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { FC } from "react";
