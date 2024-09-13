8a1fff248166b89c2a5cfa141ff9256ef3cc75c1
hir01 <glazunovden@yandex.ru>
2024-09-13 16:15:34 +0300

    search

--- src/components/HeaderSearch.tsx
+++ src/components/HeaderSearch.tsx
@@ -5,30 +5,36 @@ import { useEffect, useRef, useState } from "react";
 import { api } from "@/services/api";
 import { debounce } from "lodash-es";
 import { Product } from "@/common/types/Product";
+import { useClickAway } from "react-use";
+import Link from "next/link";
 
 export const HeaderSearch = () => {
   const [searchQuery, setSearchQuery] = useState("");
-  const [searchData, setSearchData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [searchData, setSearchData] = useState<Product[]>([]);
+  const [focused, setFocused] = useState(false);
+  const ref = useRef(null);
 
   const search = async (data: any) => {
     try {
       const fdata = await api.getSearchResult({ search: data });
-      if (fdata.length < 1) {
-        setIsLoading(true);
-        setSearchData([]);
-      } else {
-        setSearchData(fdata || []);
-      }
+      setSearchData(fdata || []);
     } catch (error: any) {
       setSearchData([]);
       console.error("Error fetching:", error.message);
       throw error;
-    } finally {
-      setIsLoading(false);
     }
   };
-  const debounced = useRef(debounce(search, 500));
+  const debounced = useRef(debounce(search, 300));
+
+  useClickAway(ref, () => {
+    setFocused(false);
+  });
+
+  const onClickItem = () => {
+    setFocused(false);
+    setSearchQuery("");
+    setSearchData([]);
+  };
 
   useEffect(() => {
     debounced.current(searchQuery);
@@ -38,12 +44,13 @@ export const HeaderSearch = () => {
   // Очистить поисковую строку при клике на результат
 
   return (
-    <div className="header__search">
+    <div className="search" ref={ref}>
       <label>
         <input
           type="text"
           name="search"
           placeholder="Поиск товара"
+          onFocus={() => setFocused(true)}
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
@@ -51,19 +58,40 @@ export const HeaderSearch = () => {
           <IconMagnifying />
         </button>
       </label>
-      <div>
-        {isLoading ? (
-          "Загрузка"
-        ) : (
-          <div>
-            {searchData.map((item: Product, index) => (
-              <p key={index}>
-                {item?.id} - {item?.productname}
-              </p>
+      {searchData.length > 0 && (
+        <div className={`search__results ${focused ? "active" : ""}`}>
+          <div className="search__wrapper">
+            {searchData.map((item) => (
+              <Link
+                onClick={onClickItem}
+                key={item.id}
+                href={`/products/${item.id}`}
+                className="search__link"
+              >
+                <picture>
+                  <source
+                    srcSet={
+                      item.image
+                        ? `https://my.loaderpro.ru/images/products/${item.image}`
+                        : "https://my.loaderpro.ru/images/no-photo.svg"
+                    }
+                  />
+                  <img
+                    src={
+                      item.image
+                        ? `https://my.loaderpro.ru/images/products/${item.image}`
+                        : "https://my.loaderpro.ru/images/no-photo.svg"
+                    }
+                    alt=""
+                    decoding="async"
+                  />
+                </picture>
+                <span>{item.productname}</span>
+              </Link>
             ))}
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
