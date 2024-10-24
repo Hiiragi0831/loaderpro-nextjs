b0936f6261b207bf3e3d1c8effb33a17bd060c86
hir01 <glazunovden@yandex.ru>
2024-10-24 11:42:21 +0300

    fix

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,9 +1,12 @@
 import Content from "./Content";
+import { Suspense } from "react";
 
 export default function SelectionParts() {
   return (
     <main>
-      <Content />
+      <Suspense>
+        <Content />
+      </Suspense>
     </main>
   );
 }
--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import Content from "./Content";
 
 export default function Page() {
   return (
     <main>
       <section className="empty">
-        <Content />
+        <Suspense>
+          <Content />
+        </Suspense>
       </section>
     </main>
   );
