166ad17497f010a035358aa8adc7f6bcf382b478
Hiiragi0831 <glazunovden@yandex.ru>
2024-11-11 14:23:04 +0300

    fix

--- src/app/products/components/ProductInfo.tsx
+++ src/app/products/components/ProductInfo.tsx
@@ -105,7 +105,7 @@ export default function ProductInfo(params: any) {
     }
   };
 
-  useLayoutEffect(() => void loadArticle());
+  useLayoutEffect(() => void loadArticle(), []);
 
   return (
     <>
