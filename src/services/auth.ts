0b63b7f48851b3adc79a1916be302cb4360a8436
denisglazunov <glazunovden@yandex.ru>
2025-01-30 16:46:15 +0300

    auth base64

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,23 +2,29 @@ import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  // session: {
-  //   strategy: "jwt",
-  // },
+  session: {
+    strategy: "jwt",
+  },
   providers: [
     Credentials({
       name: "credentials",
       credentials: {
         email: { label: "Email" },
-        password: { label: "Password", type: "password" }, // base64
+        password: { label: "Password", type: "password" },
       },
-      async authorize(credentials) {
+      authorize: async function (credentials) {
         const res = await fetch("https://api.cartrac.ru/Main/auth_user/", {
-          method: "POST",
-          body: JSON.stringify(credentials),
-          headers: { "Content-Type": "application/json" },
+          headers: {
+            Authorization:
+              "Basic " +
+              Buffer.from(
+                credentials.email + ":" + credentials.password,
+              ).toString("base64"),
+          },
         });
         const user = await res.json();
+
+        console.log(user);
 
         // If no error and we have user data, return it
         if (res.ok && user) {
