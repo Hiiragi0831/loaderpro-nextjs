311a0a178a5f078c258127ab1a4a9b5a7dec2e40
denisglazunov <glazunovden@yandex.ru>
2025-04-25 17:45:14 +0300

    страница бренда кальмар

---
+++ src/app/brand/components/Box.tsx
@@ -0,0 +1,12 @@
+export default function Box(data: { title: any; icon?: any; text: string }) {
+  return (
+    <div className={"brand-box"}>
+      {data.icon ? <div className="brand-box__icon">{data.icon}</div> : ""}
+      <p
+        className="brand-box__title"
+        dangerouslySetInnerHTML={{ __html: data.title }}
+      />
+      <p>{data.text}</p>
+    </div>
+  );
+}
